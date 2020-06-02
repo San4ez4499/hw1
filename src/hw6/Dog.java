a76e2021d877a5e1d221ee8d3cef370363a4c69e
Александр <degtyarev.alexandex@yandex.ru>
2020-06-02 12:03:46 +0300

    hw6

--- src/hw6/Dog.java
+++ src/hw6/Dog.java
@@ -1,4 +1,25 @@
 package hw6;
 
-public class Dog {
+public class Dog extends Animal {
+    private final int MAX_RUN_LENGTH = 500;
+    private final int MAX_SWIM_LENGTH = 10;
+
+    public Dog(String name, String color, int age) {
+        super(name, color, age);
+    }
+
+    @Override
+    void run(int length) {
+        if ((length >= 0) && (length <= MAX_RUN_LENGTH)) {
+            System.out.println(name + " пробежал " + MAX_RUN_LENGTH + " метров");
+        }
+    }
+
+    @Override
+    void swim(int length) {
+        if ((length >= 0) && (length <= MAX_SWIM_LENGTH)) {
+            System.out.println(name + " проплыл " + MAX_SWIM_LENGTH + " метров");
+        }
+    }
+
 }
