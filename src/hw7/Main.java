87e3cb1a0dd8e5e6a4da7ce7ec11e18c6e22c610
Александр <degtyarev.alexandex@yandex.ru>
2020-06-08 11:23:58 +0300

    hw7 finish

--- src/hw7/Main.java
+++ src/hw7/Main.java
@@ -1,7 +1,32 @@
 package hw7;
 
 public class Main {
-    public static void main(String[] args) {
+    private static int TIME = 0;
 
+    public static void main(String[] args) {
+        Cat[] cat = new Cat[3];
+        cat[0] = new Cat("Барсик", 150, 3);
+        cat[1] = new Cat("Рыжик", 100, 4);
+        cat[2] = new Cat("Пушок", 30, 1);
+        Plate plate = new Plate(700);
+        System.out.println("Привет! У нас есть три кота: " + cat[0].getName() + ", " + cat[1].getName() + " и " + cat[2].getName() + ", которые хотят есть каждые " + cat[0].getSatietyTime() + ", " + cat[1].getSatietyTime() + " и " + cat[2].getSatietyTime() + " час(а) соответственно.");
+        System.out.println("Кто-то из них более прожорлив, кто-то менее. Сейчас в миске " + plate.getFood() + " грамм кошачьего корма. Посмотрим насколько его хватит\n");
+
+        do {
+            for (Cat i : cat) {
+                if (i.getSatiety() == 0) {
+                    if (!plate.checkFood(i.getAppetite())) {
+                        plate.increaseFood();
+                    }
+                    i.eat(plate);
+                    System.out.println("Кот " + i.getName() + " съел " + i.getAppetite() + " граммов корма и проголодается через " + (i.getSatiety()) + " часа(ов)");
+                }
+                i.setSatiety(i.getSatiety() - 1);
+            }
+            System.out.println("\nС момента начала кормежки прошел(ло) " + TIME + " час(а). В миске осталось " + plate.getFood() + " граммов корма.\n");
+            TIME++;
+
+        } while (TIME <= 24);
     }
 }
+
